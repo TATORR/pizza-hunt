701f7df30de2ac0a644e20abd49e5c693fb40bfe
Trill <ty3torr3s@icloud.com>
2021-12-16 17:07:25 -0500

    finish 18.2

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,8 @@
 const router = require('express').Router();
+const commentRoutes = require('./comment-routes');
 const pizzaRoutes = require('./pizza-routes');
 
-// add prefix of `/pizzas to routes creates in pizza routes.js`
+router.use('/comments', commentRoutes);
 router.use('/pizzas', pizzaRoutes);
-
-
-
 
 module.exports = router;
